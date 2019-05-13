456bf52428b86f9bc5241df16c1dc5486bc1f0d3
juansanluq <juansanluq@gmail.com>
2019-05-13 14:58:28 +0200

    no message

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
+import Formulario from './components/Formulario';
+import Error from './components/Error';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      consulta: {}
+    }
+  }
+
+  componentDidMount() {
+    this.setState({
+      error: false,
+    })
+  }
+
+  componentDidUpdate() {
+    this.consultarApi();
+  }
+
+  consultarApi = () => {
+    const { ciudad, pais } = this.state.consulta;
+    if (!ciudad || !pais) return null;
+
+    const API_KEY = '9a5aec2c67e03e43130b37b0ecd28cb7';
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${API_KEY}`;
+    console.log(url);
+  }
+
+  datosConsulta = respuesta => {
+    if (respuesta.ciudad === '' || respuesta.pais === '') {
+      this.setState({ error: true });
+    } else {
+      this.setState({
+        error: false,
+        consulta: respuesta
+      });
+    }
+  }
+
+  render() {
+    const error = this.state.error;
+
+    let resultado;
+
+    if (error) {
+      resultado = <Error mensaje="Ambos campos son obligatorios" />
+    }
+    return (
+      <div className="app">
+        <Header
+          titulo={'Clima React'}
+        />
+        <Formulario datosConsulta={this.datosConsulta} />
+        {resultado}
+      </div>
+    );
+  }
 }
 
 export default App;
