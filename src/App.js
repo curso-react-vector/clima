a42c647d64878ac611afb7c39da12fa2b73904a9
juansanluq <juansanluq@gmail.com>
2019-05-14 13:34:03 +0200

    no message

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import Header from './components/Header';
 import Formulario from './components/Formulario';
 import Error from './components/Error';
+import Clima from './components/Clima';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       error: null,
-      consulta: {}
+      consulta: {},
+      resultado: {}
     }
   }
 
@@ -18,17 +20,32 @@ class App extends React.Component {
     })
   }
 
-  componentDidUpdate() {
-    this.consultarApi();
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.consulta !== this.state.consulta) {
+      this.consultarApi();
+    }
   }
 
   consultarApi = () => {
     const { ciudad, pais } = this.state.consulta;
     if (!ciudad || !pais) return null;
 
-    const API_KEY = '9a5aec2c67e03e43130b37b0ecd28cb7';
+    const API_KEY = 'e9439f4a7e29bc4fb99f0ddfb0070754';
     let url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${API_KEY}`;
     console.log(url);
+
+    fetch(url)
+      .then(response => {
+        return response.json();
+      })
+      .then(datos => {
+        this.setState({
+          resultado: datos,
+        })
+      })
+      .catch(error => {
+        console.log(error);
+      })
   }
 
   datosConsulta = respuesta => {
@@ -49,6 +66,8 @@ class App extends React.Component {
 
     if (error) {
       resultado = <Error mensaje="Ambos campos son obligatorios" />
+    } else {
+      resultado = <Clima resultado={this.state.resultado} />
     }
     return (
       <div className="app">
